f2836737f4791ca6cd0bd384f7b564e53077d284
johnneychen <mzchenca@gmail.com>
2021-04-12 13:57:16 -0600

    switched to local skaffold deployment

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = () => {
   return <h1>Landing page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("ticketing.dev/api/users/currentuser");
+
+  return response.data;
+};
+
+export default LandingPage;
